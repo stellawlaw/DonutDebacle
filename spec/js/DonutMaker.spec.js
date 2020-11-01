302b57c9b20125ea123f52d02eae8d511bc3ac6e
stella.w.law@gmail.com <stella.w.law@gmail.com>
2020-11-01 14:28:20 -0500

    Finished last two features of iteration 1

--- spec/js/DonutMaker.spec.js
+++ spec/js/DonutMaker.spec.js
@@ -6,9 +6,9 @@ describe('FEATURE : Have a way to count donuts', () => {
             underTest = new DonutMaker;
         });
 
-        it('Should start with a donut count of 0.', () =>{
-      
-            expect (underTest._donutCount).toBe(0);
+        it('Should start with a donut count of 0.', () => {
+
+            expect(underTest._donutCount).toBe(0);
         });
 
         it('Should add one click to the click count when it records a click.', () => {
@@ -18,7 +18,7 @@ describe('FEATURE : Have a way to count donuts', () => {
         });
 
         it('Should have a click count of 2 when it records 2 clicks.', () => {
-            
+
             underTest.recordClick();
             underTest.recordClick();
             expect(underTest._donutCount).toBe(2);
@@ -28,30 +28,36 @@ describe('FEATURE : Have a way to count donuts', () => {
 
 });
 
-describe('FEATURE : Be able to purchase the first Auto Clicker with 100 donuts from your donut count.', () => {        
-    describe('Can add to auto clicker count', () => {            
-        let underTest;
-                
-        beforeEach(() => {                
-            underTest = new DonutMaker;            
+describe('FEATURE : Be able to purchase the first Auto Clicker with 100 donuts from your donut count.', () => {
+    describe('Can add to auto clicker count', () => {
+        let underTest;
+
+        beforeEach(() => {
+            underTest = new DonutMaker;
         });
-                
-        it('Should start with an auto clicker count of 0.', () =>{               
-             expect(underTest._autoClickerCount).toBe(0);            
+
+        it('Should start with an auto clicker count of 0.', () => {
+            expect(underTest._autoClickerCount).toBe(0);
+        });
+
+        it('Should be able record an autoclick', () => {
+            underTest.recordAutoClick();
+            expect(undertest._donutCount).toBe(1);
+
         });
 
     });
 });
 
-describe('FEATURE : The cost of each Auto Clicker will go up with each purchase.', () =>{
+describe('FEATURE : The cost of each Auto Clicker will go up with each purchase.', () => {
     let underTest;
     beforeEach(() => {
         underTest = new DonutMaker;
         underTest.stashDonutsForTesting();
 
     });
-    
-    it('Should increase the cost of the second Auto Clicker by an additional ten percent.', () =>{
+
+    it('Should increase the cost of the second Auto Clicker by an additional ten percent.', () => {
         underTest.subtractAutoClickerPurchase();
         expect(underTest.autoClickerCost).toBe(100);
         expect(underTest._autoClickerCount).toBe(1);
@@ -60,7 +66,7 @@ describe('FEATURE : The cost of each Auto Clicker will go up with each purchase.
         expect(underTest._donutCount).toBe(790);
     });
 
-    it('Should increase the cost of subsequent Auto Clicker by an additional ten percent', () =>{
+    it('Should increase the cost of subsequent Auto Clicker by an additional ten percent', () => {
         underTest.subtractAutoClickerPurchase();
         underTest.subtractAutoClickerPurchase();
         underTest.subtractAutoClickerPurchase();
@@ -71,21 +77,58 @@ describe('FEATURE : The cost of each Auto Clicker will go up with each purchase.
 
 });
 
-// describe('FEATURE : Ensure that there are enough clicks to buy a Auto Clicker.', () =>{
-//     underTest = new DonutMaker;
-//     it('Should prevent Auto Clicker count from going up if there are not enough clicks to purchase a autoclicker', () => {
-//         underTest.recordClick();
-//         underTest.recordClick();
-//         underTest.recordClick();
-//         underTest.recordClick();
-//         underTest.recordClick();
-//         underTest.recordClick();
-//         expect(underTest._donutCount).toBe(5);
-//         expect(underTest._autoClickerCount).toBe(0);
-        
-//     });
+describe('Iteration 1-FEATURE 4 : Ensure that there are enough clicks to buy a Auto Clicker.', () => {
+    let underTest;
+    beforeEach(() => {
+        underTest = new DonutMaker;
+        underTest.subtractAutoClickerPurchase;
+    });
 
-// });
+    it('Should prevent Auto Clicker count from going up if there are not enough clicks to purchase a autoclicker', () => {
+        underTest.recordClick();
+        underTest.recordClick();
+        underTest.recordClick();
+        underTest.recordClick();
+        underTest.recordClick();
+        underTest.subtractAutoClickerPurchase();
+        expect(underTest._donutCount).toBe(5);
+        expect(underTest._autoClickerCount).toBe(0);
+        underTest.stashDonutsForTesting();
+        underTest.subtractAutoClickerPurchase();
+        expect(underTest._donutCount).toBe(905);
+        expect(underTest._autoClickerCount).toBe(1);
+    });
+
+});
+
+describe('Iteration 1-FEATURE 5 : The amount of Auto Clickers affect the amount of donuts added when an Activate Auto Clickers event is called.', () => {
+    let underTest;
+    beforeEach(() => {
+        underTest = new DonutMaker;
+        underTest.stashAutoClickersForTesting();
+    });
+
+    it('Should increase the donut total by the amount of Auto Clickers owned when activate Auto Clickers is executed.', () => {
+        underTest.activateAutoClicker();
+        underTest.recordClick();
+        expect(underTest._donutCount).toBe(11);
+    });
+});
+
+describe('Iteration 2- FEATURE 1 : Be able to purchase the first Donut Multiplier with 10 clicks from your click count.', () => {
+    let underTest;
+    beforeEach(() => {
+        underTest = new DonutMaker;
+    });
+
+    it('Should be able to retrieve a Donut Multiplier count', () => {
+        
+    });
+    
+    
+});
+
+
 
 
 
--- src/js/DonutMaker.js
+++ src/js/DonutMaker.js
@@ -1,59 +1,65 @@
-class DonutMaker{
+class DonutMaker {
 
-    constructor(){
-        this._donutCount=0;
-        this._autoClickerCount= 0;
-        this._baseAutoClickerCost= 100;
-    }
-
-    stashDonutsForTesting(){
-        this._donutCount+=1000;
-    }
-
-    stashAutoClickersForTesting(){
-        this.autoClickerCount+=10;
-    }
-
-    recordClick(){
-        this._donutCount++;
-    }
-
-    get donutCount(){
-        return this._donutCount;
-    }
-
-    subtractAutoClickerPurchase(){
-        if(this._autoClickerCount==0){
-            this.autoClickerCost =100;
-        }
-        else{
-            let n = this._autoClickerCount;
-            this.autoClickerCost = Math.round(100*Math.pow(1.1, n));
-        }
-        this._donutCount-=this.autoClickerCost;
-        this._autoClickerCount++;
-
-        // if(this._donutCount>=this.autoClickerCost){
-        //     this._donutCount-=this.autoClickerCost;
-        //      this._autoClickerCount++;
-        // }
-        // else{
-        //     this._donutCount=this._donutCount;
-        // }
+    constructor() {
+        this._donutCount = 0;
+        this._autoClickerCount = 0;
         
     }
 
+    stashDonutsForTesting() {
+        this._donutCount += 1000;
+    }
 
-  
+    stashAutoClickersForTesting() {
+        this._autoClickerCount += 10;
+    }
+    get autoClickerCount() {
+        return this._autoClickerCount;
+    }
 
+    recordClick() {
+        this._donutCount++;
+    }
+    recordAutoClick() {
+        this._donutCount++;
+    }
 
+    recordDonutMultiplier(){
+        this.donutMultiplier++;
+    }
 
-    get autoClickerCount(){        
-        return this._autoClickerCount;    }
+    get donutCount() {
+        return this._donutCount;
+    }
 
-   
+    subtractAutoClickerPurchase() {
+        if (this._autoClickerCount == 0) {
+            this.autoClickerCost = 100;
+        }
+        else {
+            let n = this._autoClickerCount;
+            this.autoClickerCost = Math.round(100 * Math.pow(1.1, n));
+        }
 
+        if (this._donutCount >= this.autoClickerCost) {
+            this._donutCount -= this.autoClickerCost;
+            this._autoClickerCount++;
+        }
+        else {
+            this._donutCount = this._donutCount;
+        }
+    }
+
+    activateAutoClicker() {
+        this._donutCount = this._donutCount + (this._autoClickerCount);
+
+    }
+
+    
 }
+
+
+
 
 
 
