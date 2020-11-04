f80a5d99943fa9bd86f0f25ea1c149bf8b4f5704
stella.w.law@gmail.com <stella.w.law@gmail.com>
2020-11-03 19:21:13 -0500

    added some styling and javascript pop up for company information in header

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+document.getElementById("company-info").addEventListener("click", companyinfofunction);
+
+function companyinfofunction(){
+    alert("This is going to be the company information that I make up");
+}
